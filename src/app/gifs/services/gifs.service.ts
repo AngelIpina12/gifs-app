884793a9e1c82d153cf5fa2f56768645f4ad64f1
AngelIpina12 <angelipina12@hotmail.com>
2024-09-18 22:07:57 -0600

    Usando localStorage para almacenar historial y cargar ultima busqueda de gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,7 +9,9 @@ export class GifsService {
   private apiKey: string = 'YfKanqyOTEX9DgABdUFmTM48tILmkfXs'
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage()
+   }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -22,6 +24,19 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0])
+
   }
 
   public searchTag(tag: string):void{
